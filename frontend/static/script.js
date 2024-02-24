97700ddacd80525e8453c89476f61ef3a4030682
Austin Snyder <Austin.Snyder4114@gmail.com>
2024-02-23 22:15:43 -0800

    groundwork for editing/deleting applications

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -21,12 +21,43 @@ let handle_applications = (apps) =>{
                          <td>${app[3]}</td>
                          <td>${app[4]}</td>
                          <td>${app[5]}</td>
-                        <td><button value = "Delete" id=${app[0]}></td>
-                        <td><button value = "Edit" id=${app[0]}></td>`;
+                        <td><button value = "Delete" id=${app[0]} class="delete"></td>
+                        <td><button value = "Edit" id=${app[0]+10} class="edit"></td>`;
         tbody.appendChild(row);
     })
 
-    document.querySelectorAll("")
+    let deleteButtons = document.querySelectorAll(".delete");
+    let editButtons = document.querySelectorAll(".edit");
+
+    edit_delete_listeners(deleteButtons, editButtons)
+}
+
+let edit_delete_listeners = (deleteButtons, editButtons) => {
+    deleteButtons.forEach(button =>{
+        let id = button.getAttribute("id")
+        button.addEventListener("click", () => delete_app(id));
+    });
+
+    editButtons.forEach(button =>{
+        let id = button.getAttribute("id")
+        button.addEventListener("click", () => edit_app(id));
+    });
+}
+
+let edit_app = (id) =>{
+
+}
+
+let delete_app = (id) =>{
+     const isConfirmed = window.confirm("Are you sure you want to delete this application?\nThis action CANNOT be undone!");
+
+    // Check if the user confirmed the deletion
+    if (isConfirmed) {
+        console.log(`Deleting application with ID: ${id}`);
+    } else {
+        // The user clicked 'Cancel' or closed the dialog
+        console.log('Deletion canceled');
+    }
 }
 
 //Fetches the applications from python file.
